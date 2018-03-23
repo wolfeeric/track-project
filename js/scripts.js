bc777fef278dd57064c4100d636e2db49c2d5909
xerionxavier <phrak9@yahoo.com>
2018-03-23 11:31:43 -0700

    Fleshing out content, columns, rows js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
 
   $("#frontEnd").click(function() {
-     $("#choice2.hidden").show();
+     $("#choice2").toggleClass();
      $("#choice3").hide();
-   });
+  });
 
+  $("#backEnd").click(function() {
+    $("#choice3").toggleClass();
+    $("#choice2").hide();
+  });
+
+  $("#html").click(function() {
+    $("#choice4").toggleClass();
+    $("#choice5").hide();
+  });
+
+  $("#css").click(function() {
+    $("#choice5").toggleClass();
+    $("#choice4").hide();
+  });
+
+  $("#Javascript").click(function() {
+    $("#choice5").toggleClass();
+    $("#choice4").hide();
+  });
 });
